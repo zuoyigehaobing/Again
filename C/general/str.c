4af6a55e4c49ac453c389564f5be83c10100cf5f
Aaron Shan <zuoyigehaobing@gmail.com>
2016-11-06 19:34:51 -0500

    local change

--- C/general/str.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	char *sweet = malloc(20* sizeof(char));
-	*sweet = "tiramisu";
-	printf("%s",*sweet);
-	return 0;
-}
--- C/general/trash/compile.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-	int * p;
-	int a = 9;
-	p = &a;
-	char * my_str = "nihao";
-	p = my_str;
-	return 0;
-}
